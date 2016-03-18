1a8f31b0d9acd2b2db977878ff7004b6047e8ef3
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-03-18 18:19:06 +0200

    core: clean up (golint)

--- src/internal/core/core.go
+++ src/internal/core/core.go
@@ -27,72 +27,84 @@ func Info(b []byte) (interface{}, error) {
 	return parseInfo(b)
 }
 
+// GetAuth returns slice of things or nil instead ones (if thing doesn't exist).
 func GetAuth(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeAuth(data).get(c)
 }
 
+// SetAuth upserts things and returns simple "OK".
 func SetAuth(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeAuth(data).set(c)
 }
 
+// DelAuth deletes things and returns the number of things that were removed.
 func DelAuth(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeAuth(data).del(c)
 }
 
+// GetLinkAddr returns slice of things or nil instead ones (if thing doesn't exist).
 func GetLinkAddr(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkAddr(data).get(c)
 }
 
+// SetLinkAddr upserts things and returns simple "OK".
 func SetLinkAddr(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkAddr(data).set(c)
 }
 
+// DelLinkAddr deletes things and returns the number of things that were removed.
 func DelLinkAddr(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkAddr(data).del(c)
 }
 
+// GetLinkDrug returns slice of things or nil instead ones (if thing doesn't exist).
 func GetLinkDrug(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkDrug(data).get(c)
 }
 
+// SetLinkDrug upserts things and returns simple "OK".
 func SetLinkDrug(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkDrug(data).set(c)
 }
 
+// DelLinkDrug deletes things and returns the number of things that were removed.
 func DelLinkDrug(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkDrug(data).del(c)
 }
 
+// GetLinkStat returns slice of things or nil instead ones (if thing doesn't exist).
 func GetLinkStat(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkStat(data).get(c)
 }
 
+// SetLinkStat upserts things and returns simple "OK".
 func SetLinkStat(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
 	return decodeLinkStat(data).set(c)
 }
 
+// DelLinkStat deletes things and returns the number of things that were removed.
 func DelLinkStat(data []byte) (interface{}, error) {
 	c := redisPool.Get()
 	defer redisPool.Put(c)
