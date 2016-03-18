a6b7bfd0a01d592b529d9b31ab13df979dc19733
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-03-18 17:27:09 +0200

    core: clean up (deadcode)

--- src/internal/core/util.go
+++ src/internal/core/util.go
@@ -3,7 +3,6 @@ package core
 import (
 	"bufio"
 	"bytes"
-	"encoding/json"
 	"strings"
 
 	"internal/errors"
@@ -18,16 +17,6 @@ func valsFromStrings(v []string) []interface{} {
 	return res
 }
 
-// JSON -> []string -> []interface{}
-func valsFromJSONStrings(b []byte) ([]interface{}, error) {
-	var v []string
-	err := json.Unmarshal(b, &v)
-	if err != nil {
-		return nil, errors.Locus(err)
-	}
-	return valsFromStrings(v), nil
-}
-
 // []int64 -> []interface{}
 func valsFromInt64s(v []int64) []interface{} {
 	res := make([]interface{}, 0, len(v))
@@ -35,16 +24,6 @@ func valsFromInt64s(v []int64) []interface{} {
 		res = append(res, v[i])
 	}
 	return res
-}
-
-// JSON -> []int64 -> []interface{}
-func valsFromJSONInt64s(b []byte) ([]interface{}, error) {
-	var v []int64
-	err := json.Unmarshal(b, &v)
-	if err != nil {
-		return nil, errors.Locus(err)
-	}
-	return valsFromInt64s(v), nil
 }
 
 // string -> [0]interface{}
@@ -65,24 +44,6 @@ func makeVector(cmd, key string, v ...interface{}) []interface{} {
 	}
 	res := make([]interface{}, 0, len(v)+m)
 	return append(append(res, cmd), mergeKeyVals(key, v...)...)
-}
-
-// cmd key -> mem1
-// cmd key -> mem2
-// cmd key -> mem3
-func makeBulkOne(cmd, key string, v ...interface{}) [][]interface{} {
-	res := make([][]interface{}, 0, len(v))
-	for i := range v {
-		res = append(res, makeVector(cmd, key, v[i]))
-	}
-	return res
-}
-
-// cmd key1 -> [key1:] mem1 mem2 mem3 mem4
-// cmd key2 -> [key2:] mem1 mem2 mem3 mem4
-// cmd key3 -> [key3:] mem1 mem2 mem3 mem4
-func makeBulkVector(src [][]interface{}, cmd, key string, v ...interface{}) [][]interface{} {
-	return append(src, makeVector(cmd, key, v...))
 }
 
 // TODO: parse keyspace
