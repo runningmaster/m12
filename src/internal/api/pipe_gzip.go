7c58afcd376fe6273d0dccdde743122ef172395e
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-04-06 15:17:14 +0300

    internal: add compress/gzip

--- src/internal/api/pipe_gzip.go
+++ src/internal/api/pipe_gzip.go
@@ -2,54 +2,24 @@ package api
 
 import (
 	"bufio"
-	"bytes"
 	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"strings"
-	"sync"
 
+	gz "internal/compress/gzip"
 	"internal/errors"
 
 	"github.com/klauspost/compress/gzip"
 	"golang.org/x/net/context"
 )
 
-var (
-	// gzipSimple is gzip empty string for init reader
-	gzipSimple = []byte{
-		0x1f, 0x8b, 0x8, 0x0, 0x0, 0x9, 0x6e, 0x88, 0x0, 0xff, 0x1,
-		0x0, 0x0, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-	}
-
-	gzipReaderPool = sync.Pool{
-		New: func() interface{} {
-			z, err := gzip.NewReader(bytes.NewReader(gzipSimple))
-			if err != nil {
-				return errors.Locus(err)
-			}
-			return z
-		},
-	}
-
-	gzipWriterPool = sync.Pool{
-		New: func() interface{} {
-			z, err := gzip.NewWriterLevel(ioutil.Discard, gzip.DefaultCompression)
-			if err != nil {
-				return errors.Locus(err)
-			}
-			return z
-		},
-	}
-)
-
-type gzipWriter struct {
+type gzWriter struct {
 	io.Writer
 	http.ResponseWriter
 }
 
-func (w gzipWriter) Write(b []byte) (int, error) {
+func (w gzWriter) Write(b []byte) (int, error) {
 	if w.Header().Get("Content-Type") == "" {
 		w.Header().Set("Content-Type", http.DetectContentType(b))
 	}
@@ -62,11 +32,11 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 	return n, nil
 }
 
-func (w gzipWriter) Flush() error {
+func (w gzWriter) Flush() error {
 	return errors.Locus(w.Writer.(*gzip.Writer).Flush())
 }
 
-func (w gzipWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+func (w gzWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	c, rw, err := w.ResponseWriter.(http.Hijacker).Hijack()
 	if err != nil {
 		return c, rw, errors.Locus(err)
@@ -75,7 +45,7 @@ func (w gzipWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	return c, rw, nil
 }
 
-func (w *gzipWriter) CloseNotify() <-chan bool {
+func (w *gzWriter) CloseNotify() <-chan bool {
 	return w.ResponseWriter.(http.CloseNotifier).CloseNotify()
 }
 
@@ -89,61 +59,29 @@ func gzipInAcceptEncoding(r *http.Request) bool {
 func pipeGzip(h handlerFunc) handlerFunc {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		if gzipInContentEncoding(r) {
-			switch z := gzipReaderPool.Get().(type) {
-			case *gzip.Reader:
-				defer func() {
-					_ = z.Close()
-					gzipReaderPool.Put(z)
-				}()
-				if err := z.Reset(r.Body); err != nil {
-					// FIXME TODO log err
-				}
-				r.Body = z
-			case error:
-				panic(z) // FIXME TODO
+			z, err := gz.GetReader()
+			if err != nil {
+				// FIXME log err
 			}
+			defer gz.CloseReader(z)
+			if err := z.Reset(r.Body); err != nil {
+				// FIXME log err
+			}
+			r.Body = z
 		}
 
 		if gzipInAcceptEncoding(r) {
-			switch z := gzipWriterPool.Get().(type) {
-			case *gzip.Writer:
-				defer func() {
-					_ = z.Close()
-					gzipWriterPool.Put(z)
-				}()
-				z.Reset(w)
-				w = gzipWriter{Writer: z, ResponseWriter: w}
-				w.Header().Add("Vary", "Accept-Encoding")
-				w.Header().Set("Content-Encoding", "gzip")
-			case error:
-				panic(z) // FIXME TODO
+			z, err := gz.GetWriter()
+			if err != nil {
+				// FIXME TODO log err
 			}
+			defer gz.CloseWriter(z)
+			z.Reset(w)
+			w = gzWriter{Writer: z, ResponseWriter: w}
+			w.Header().Add("Vary", "Accept-Encoding")
+			w.Header().Set("Content-Encoding", "gzip")
 		}
 
 		h(ctx, w, r)
 	}
-}
-
-func gunzip(data []byte) ([]byte, error) {
-	switch z := gzipReaderPool.Get().(type) {
-	case *gzip.Reader:
-		defer func() {
-			_ = z.Close()
-			gzipReaderPool.Put(z)
-		}()
-
-		if err := z.Reset(bytes.NewReader(data)); err != nil {
-			return nil, errors.Locus(err)
-		}
-
-		out, err := ioutil.ReadAll(z)
-		if err != nil {
-			return nil, errors.Locus(err)
-		}
-
-		return out, nil
-	case error:
-		return nil, z
-	}
-	return nil, errors.Locusf("gunzip: unreachable")
 }
---
+++ src/internal/compress/gzip/gzip.go
@@ -0,0 +1,93 @@
+package gzip
+
+import (
+	"bytes"
+	"io/ioutil"
+	"sync"
+
+	"internal/errors"
+
+	"github.com/klauspost/compress/gzip"
+)
+
+var (
+	// simple is gzip empty string for init reader
+	simple = []byte{
+		0x1f, 0x8b, 0x8, 0x0, 0x0, 0x9, 0x6e, 0x88, 0x0, 0xff, 0x1,
+		0x0, 0x0, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+	}
+
+	readerPool = sync.Pool{
+		New: func() interface{} {
+			z, err := gzip.NewReader(bytes.NewReader(simple))
+			if err != nil {
+				return errors.Locus(err)
+			}
+			return z
+		},
+	}
+
+	writerPool = sync.Pool{
+		New: func() interface{} {
+			z, err := gzip.NewWriterLevel(ioutil.Discard, gzip.DefaultCompression)
+			if err != nil {
+				return errors.Locus(err)
+			}
+			return z
+		},
+	}
+)
+
+// GetReader gets reader from pool.
+func GetReader() (*gzip.Reader, error) {
+	switch r := readerPool.Get().(type) {
+	case *gzip.Reader:
+		return r, nil
+	case error:
+		return nil, r
+	}
+	return nil, errors.Locusf("gzip: GetReader(): unreachable")
+}
+
+// CloseReader closes reader and puts it back to the pool.
+func CloseReader(r *gzip.Reader) {
+	_ = r.Close()
+	readerPool.Put(r)
+}
+
+// GetWriter gets writer from pool.
+func GetWriter() (*gzip.Writer, error) {
+	switch w := readerPool.Get().(type) {
+	case *gzip.Writer:
+		return w, nil
+	case error:
+		return nil, w
+	}
+	return nil, errors.Locusf("gzip: GetWriter(): unreachable")
+}
+
+// CloseWriter closes writer and puts it back to the pool.
+func CloseWriter(w *gzip.Writer) {
+	_ = w.Close()
+	writerPool.Put(w)
+}
+
+// Gunzip decodes gzip-bytes.
+func Gunzip(data []byte) ([]byte, error) {
+	z, err := GetReader()
+	if err != nil {
+		return nil, errors.Locus(err)
+	}
+	defer CloseReader(z)
+
+	if err = z.Reset(bytes.NewReader(data)); err != nil {
+		return nil, errors.Locus(err)
+	}
+
+	var out []byte
+	if out, err = ioutil.ReadAll(z); err != nil {
+		return nil, errors.Locus(err)
+	}
+
+	return out, nil
+}
