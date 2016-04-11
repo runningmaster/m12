52447f7283f8f4b1461878ad18e50d5cb4b67c54
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-04-11 12:17:59 +0300

    internal: add crypto/uuid

--- src/internal/api/pipe_head.go
+++ src/internal/api/pipe_head.go
@@ -1,57 +1,22 @@
 package api
 
 import (
-	"fmt"
 	"net"
 	"net/http"
-	"sync"
 	"time"
 
 	"internal/context/ctxutil"
+	"internal/crypto/uuid"
 
-	"github.com/rogpeppe/fastuuid"
 	"golang.org/x/net/context"
 )
-
-var uuidPool = sync.Pool{
-	New: func() interface{} {
-		g, err := fastuuid.NewGenerator()
-		if err != nil {
-			return err
-		}
-		return g
-	},
-}
-
-// GetGenerator gets generator from pool.
-func getGenerator() (*fastuuid.Generator, error) {
-	switch g := uuidPool.Get().(type) {
-	case *fastuuid.Generator:
-		return g, nil
-	case error:
-		return nil, g
-	}
-
-	return nil, fmt.Errorf("uuid: unreachable")
-}
-
-// PutGenerator puts generator back to the pool.
-func putGenerator(x *fastuuid.Generator) {
-	uuidPool.Put(x)
-}
 
 func pipeHead(h handlerFunc) handlerFunc {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		ctx = ctxutil.WithTime(ctx, time.Now())
+		ctx = ctxutil.WithID(ctx, uuid.Next())
 		ctx = ctxutil.WithIP(ctx, findIP(r))
-
-		g, err := getGenerator()
-		if err != nil {
-			// FIXME log err
-		}
-		defer putGenerator(g)
-
-		h(ctxutil.WithID(ctx, fmt.Sprintf("%x", g.Next())), w, r)
+		h(ctx, w, r)
 	}
 }
 
@@ -60,9 +25,11 @@ func findIP(r *http.Request) string {
 	if ip = r.Header.Get("X-Real-IP"); ip == "" {
 		ip = r.Header.Get("X-Forwarded-For")
 	}
+
 	if ip == "" {
 		ip = r.RemoteAddr
 	}
 	ip, _, _ = net.SplitHostPort(ip)
+
 	return ip
 }
---
+++ src/internal/crypto/uuid/uuid.go
@@ -0,0 +1,43 @@
+package uuid
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/rogpeppe/fastuuid"
+)
+
+var uuidPool = sync.Pool{
+	New: func() interface{} {
+		g, err := fastuuid.NewGenerator()
+		if err != nil {
+			return err
+		}
+		return g
+	},
+}
+
+func getGenerator() (*fastuuid.Generator, error) {
+	switch g := uuidPool.Get().(type) {
+	case *fastuuid.Generator:
+		return g, nil
+	case error:
+		return nil, g
+	}
+
+	return nil, fmt.Errorf("uuid: unreachable")
+}
+
+func putGenerator(x *fastuuid.Generator) {
+	uuidPool.Put(x)
+}
+
+func Next() string {
+	g, err := getGenerator()
+	if err != nil {
+		// FIXME log err
+	}
+	defer putGenerator(g)
+
+	return fmt.Sprintf("%x", g.Next())
+}
