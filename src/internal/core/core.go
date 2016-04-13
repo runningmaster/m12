09dbc0147c7204a8b8b992a5aee656836f7614cb
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-04-13 19:42:01 +0300

    core: remove ErrFailFast

--- src/internal/core/core.go
+++ src/internal/core/core.go
@@ -6,8 +6,5 @@ import (
 	"golang.org/x/net/context"
 )
 
-// ErrFailFast informs us that work is impossible
-var ErrFailFast error
-
 // Handler is func for processing data from api.
 type Handler func(context.Context, *http.Request) (interface{}, error)
