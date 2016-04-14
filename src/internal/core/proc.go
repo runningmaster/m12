f89effd2c874c3780323be465fa2d663fc3cfcf2
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-04-15 00:53:42 +0300

    core: add proc (draft)

---
+++ src/internal/core/proc.go
@@ -0,0 +1,11 @@
+package core
+
+/*
+CheckGzip (if not then fail)
+GetHeader [if not exist then find query param (Form)]
+Calc body hash md5
+sha1:	af064923bbf2301596aac4c273ba32178ebc4a96
+md5:	b0804ec967f48520697662a204f5fe72
+Put "H.Tag" "H.UUID"+.gz
+SendToChannel
+*/
