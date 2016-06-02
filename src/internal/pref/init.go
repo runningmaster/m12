933e2a1ae26d7d2687cab35c578441a13608044b
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-06-03 01:22:52 +0300

    pref: add init.go

---
+++ src/internal/pref/init.go
@@ -0,0 +1,112 @@
+package pref
+
+import (
+	"expvar"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/sasbury/mini"
+)
+
+// Init is public init func, must be called from main()
+// NOTE about priority levels:
+// 5.explicit set > 4.flag > 3.environment > 2.config > 1.key/value store > 0.default
+func Init() {
+	init0(prefs...) // 0 default
+	init1(prefs...) // 1 key/value store
+	init2(prefs...) // 2 config
+	init3(prefs...) // 3 environment
+	init4(prefs...) // 4 flag
+	init5("debug")  // 5 explicit set TEST ONLY, FIXME
+}
+
+func init0(p ...pref) {
+	// see above
+}
+
+func init1(p ...pref) {
+	// not implemented
+}
+
+func init2(p ...pref) {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	cfg, err := mini.LoadConfiguration(os.Args[1])
+	if err != nil {
+		return
+	}
+
+	for i := range p {
+		setFromConfig(p[i], cfg)
+	}
+}
+
+func init3(p ...pref) {
+	for i := range p {
+		setFromEvar(p[i])
+	}
+}
+
+func init4(p ...pref) {
+	for i := range p {
+		setFromFlag(p[i])
+	}
+	flag.Parse()
+}
+
+func init5(key string) {
+	expvar.NewInt(key).Set(1)
+	d, _ := strconv.Atoi(expvar.Get(key).String())
+	Debug = d == 1
+}
+
+func envVar(p pref) string {
+	return fmt.Sprintf(envFormat, strings.ToUpper(strings.Replace(p.name, "-", "_", -1)))
+}
+
+func setFromConfig(p pref, cfg *mini.Config) {
+	switch x := p.value.(type) {
+	case *string:
+		*x = cfg.String(p.name, *x)
+	case *bool:
+		*x = cfg.Boolean(p.name, *x)
+	default:
+		panic("pref: unreachable: config")
+	}
+}
+
+func setFromEvar(p pref) {
+	v := os.Getenv(envVar(p))
+	if v == "" {
+		return
+	}
+
+	switch x := p.value.(type) {
+	case *string:
+		*x = v
+	case *bool:
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			return
+		}
+		*x = b
+	default:
+		panic("pref: unreachable: evar")
+	}
+}
+
+func setFromFlag(p pref) {
+	switch x := p.value.(type) {
+	case *string:
+		flag.StringVar(x, p.name, *x, p.usage)
+	case *bool:
+		flag.BoolVar(x, p.name, *x, p.usage)
+	default:
+		panic("pref: unreachable: flag")
+	}
+}
--- src/internal/pref/pref.go
+++ src/internal/pref/pref.go
@@ -1,14 +1,5 @@
 package pref
 
-import (
-	"expvar"
-	"flag"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
-
 const envFormat = "M12_%s"
 
 type pref struct {
@@ -18,31 +9,31 @@ type pref struct {
 }
 
 var (
-	// Host is host server address
+	// Host is host server address.
 	Host = "http://127.0.0.1:8080"
 
-	// NATS is NATS server address
+	// NATS is NATS server address.
 	NATS = "nats://user:pass@host:4222"
 
-	// Minio is Minio server address
+	// Minio is Minio server address.
 	Minio = "http://127.0.0.1:9000"
 
-	// MinioAKey is Minio access key
+	// MinioAKey is Minio access key.
 	MinioAKey = ""
 
-	// MinioSKey is Minio secret key
+	// MinioSKey is Minio secret key.
 	MinioSKey = ""
 
-	// Redis is Redis server address
+	// Redis is Redis server address.
 	Redis = "redis://127.0.0.1:6379"
 
-	// MasterKey is default secret key for sysdba
+	// MasterKey is default secret key for sysdba.
 	MasterKey = "masterkey"
 
-	// Debug is flag for debug mode
+	// Debug is flag for debug mode.
 	Debug = false
 
-	// Verbose is flag for verbose output
+	// Verbose is flag for verbose output.
 	Verbose = true
 
 	prefs = []pref{
@@ -93,81 +84,3 @@ var (
 		},
 	}
 )
-
-// Init is public init func, must be called from main()
-// NOTE about priority levels:
-// 5.explicit set > 4.flag > 3.environment > 2.config > 1.key/value store > 0.default
-func Init() {
-	init0Defaults()
-	init1KeyValStore()
-	init2ConfigFile()
-	init3FromEvars()
-	init4FromFlags()
-	init5ExpvarFIXME("debug")
-}
-
-func init0Defaults() {
-	// see above
-}
-
-func init1KeyValStore() {
-	// not implemented
-}
-
-func init2ConfigFile() {
-	// not implemented
-}
-
-func init3FromEvars() {
-	for i := range prefs {
-		prefs[i].setFromEvar()
-	}
-}
-
-func init4FromFlags() {
-	for i := range prefs {
-		prefs[i].setFromFlag()
-	}
-	flag.Parse()
-}
-
-func init5ExpvarFIXME(key string) {
-	expvar.NewInt(key).Set(1)
-	d, _ := strconv.Atoi(expvar.Get(key).String())
-	Debug = d == 1
-}
-
-func (p pref) setFromFlag() {
-	switch x := p.value.(type) {
-	case *string:
-		flag.StringVar(x, p.name, *x, p.usage)
-	case *bool:
-		flag.BoolVar(x, p.name, *x, p.usage)
-	default:
-		panic("pref: unreachable, add new flag case")
-	}
-}
-
-func (p pref) envVar() string {
-	return fmt.Sprintf(envFormat, strings.ToUpper(strings.Replace(p.name, "-", "_", -1)))
-}
-
-func (p pref) setFromEvar() {
-	v := os.Getenv(p.envVar())
-	if v == "" {
-		return
-	}
-
-	switch x := p.value.(type) {
-	case *string:
-		*x = v
-	case *bool:
-		b, err := strconv.ParseBool(v)
-		if err != nil {
-			return
-		}
-		*x = b
-	default:
-		panic("pref: unreachable, add new evar case")
-	}
-}
