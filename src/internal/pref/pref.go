cc8b7c316e99abcb7a21e372c6e777d2fb92a068
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-06-02 21:09:15 +0300

    pref: rewrite Init()

--- src/internal/pref/pref.go
+++ src/internal/pref/pref.go
@@ -3,11 +3,19 @@ package pref
 import (
 	"expvar"
 	"flag"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
-// NOTE about priority levels:
-// 5.explicit set > 4.flag > 3.environment > 2.config > 1.key/value store > 0.default
+const envFormat = "M12_%s"
+
+type pref struct {
+	name  string
+	usage string
+	value interface{}
+}
 
 var (
 	// Host is host server address
@@ -36,61 +44,130 @@ var (
 
 	// Verbose is flag for verbose output
 	Verbose = true
+
+	prefs = []pref{
+		pref{
+			"host",
+			"Host server address",
+			&Host,
+		},
+		pref{
+			"nats",
+			"NATS server address",
+			&NATS,
+		},
+		pref{
+			"minio",
+			"Minio S3 object storage address",
+			&Minio,
+		},
+		pref{
+			"minio-akey",
+			"Minio S3 access key",
+			&MinioAKey,
+		},
+		pref{
+			"minio-skey",
+			"Minio S3 secret key",
+			&MinioSKey,
+		},
+		pref{
+			"redis",
+			"Redis server address",
+			&Redis,
+		},
+		pref{
+			"masterkey",
+			"Secret key for sysdba",
+			&MasterKey,
+		},
+		pref{
+			"debug",
+			"Debug mode",
+			&Debug,
+		},
+		pref{
+			"verbose",
+			"Verbose output",
+			&Verbose,
+		},
+	}
 )
 
-func init0() {
+// Init is public init func, must be called from main()
+// NOTE about priority levels:
+// 5.explicit set > 4.flag > 3.environment > 2.config > 1.key/value store > 0.default
+func Init() {
+	init0Defaults()
+	init1KeyValStore()
+	init2ConfigFile()
+	init3FromEvars()
+	init4FromFlags()
+	init5ExpvarFIXME("debug")
+}
+
+func init0Defaults() {
 	// see above
 }
 
-func init1() {
+func init1KeyValStore() {
 	// not implemented
 }
 
-func init2() {
+func init2ConfigFile() {
 	// not implemented
 }
 
-func init3() {
-	// not implemented
+func init3FromEvars() {
+	for i := range prefs {
+		prefs[i].setFromEvar()
+	}
 }
 
-func init4() {
-	flagString("host", "Host server address", &Host)
-	flagString("nats", "NATS server address", &NATS)
-	flagString("minio", "Minio S3 object storage address", &Minio)
-	flagString("minio-akey", "Minio S3 access key", &MinioAKey)
-	flagString("minio-skey", "Minio S3 secret key", &MinioSKey)
-	flagString("redis", "Redis server address", &Redis)
-	flagString("masterkey", "Secret key for sysdba", &MasterKey)
-	flagBool("debug", "Debug mode", &Debug)
-	flagBool("verbose", "Verbose output", &Verbose)
+func init4FromFlags() {
+	for i := range prefs {
+		prefs[i].setFromFlag()
+	}
 	flag.Parse()
 }
 
-func init5() {
-	// not implemented
-	// expvar (?)
-}
-
-func Init() {
-	init0()
-	init1()
-	init2()
-	init3()
-	init4()
-	init5()
-}
-
-func flagString(name, usage string, val *string) {
-	flag.StringVar(val, name, *val, usage)
-}
-
-func flagBool(name, usage string, val *bool) {
-	flag.BoolVar(val, name, *val, usage)
-}
-
-func experimentWithExpVarFIXME() {
-	expvar.NewInt("debug").Set(1)
-	d, _ := strconv.Atoi(expvar.Get("debug").String())
+func init5ExpvarFIXME(key string) {
+	expvar.NewInt(key).Set(1)
+	d, _ := strconv.Atoi(expvar.Get(key).String())
 	Debug = d == 1
+}
+
+func (p pref) setFromFlag() {
+	switch x := p.value.(type) {
+	case *string:
+		flag.StringVar(x, p.name, *x, p.usage)
+	case *bool:
+		flag.BoolVar(x, p.name, *x, p.usage)
+	default:
+		panic("pref: unreachable, add new flag case")
+	}
+}
+
+func (p pref) envVar() string {
+	return fmt.Sprintf(envFormat, strings.ToUpper(strings.Replace(p.name, "-", "_", -1)))
+}
+
+func (p pref) setFromEvar() {
+	v := os.Getenv(p.envVar())
+	if v == "" {
+		return
+	}
+
+	switch x := p.value.(type) {
+	case *string:
+		*x = v
+	case *bool:
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			return
+		}
+		*x = b
+	default:
+		panic("pref: unreachable, add new evar case")
+	}
 }
