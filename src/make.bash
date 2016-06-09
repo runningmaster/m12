d365726c494561aa565f04b328fd06b419eec4da
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-06-09 16:26:27 +0300

    make: add -race for build

--- src/make.bash
+++ src/make.bash
@@ -2,5 +2,5 @@
 . ../etc/env.conf
 
 go generate internal/version
-go build -i -o $GOPATH/bin/$PROJNAME main
+go build -i -o $GOPATH/bin/$PROJNAME -race main
 go list ./... | grep -v vendor/ | xargs -L1 go fmt
